34483cb4837002c5ab9da4ff11c0d1d34b14e09b
wanyong1 <chy7087114@gmail.com>
2025-05-02 19:23:37 +0900

    이동코드 수정본(1차)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 2f;
-    public float jumpForce = 5f;
+    //public float moveSpeed = 2f;
+    //public float jumpForce = 5f;
+
+    public float maxChargeTime;
+    public float maxJumpForce;
+    public float horizontalForceMultiplier;
+    public float verticalForceMultiplier;
 
     private Rigidbody2D rb;
-    private bool isJump = false;
+    private float chargeTime = 0f;
+    private bool isCharging = false;
+    private Vector2 inputDirection;
 
     void Start()
     {
@@ -15,16 +22,47 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // ÁÂ¿ì ÀÌµ¿
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        float height = Input.GetAxis("Horizontal");
+        inputDirection = new Vector2(height,1).normalized;
 
-        // Á¡ÇÁ
-        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            isJump = true;
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isCharging = true;
+            chargeTime = 0f;
         }
+
+        if (isCharging)
+        {
+            chargeTime = chargeTime + Time.deltaTime;
+            chargeTime = Mathf.Min(chargeTime, maxChargeTime);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && isCharging)
+        {
+            float powerRatio = chargeTime / maxChargeTime;
+
+            // x ÈûÀº ÀÛ°Ô, y ÈûÀº ¼¼°Ô
+            Vector2 jumpForce = new Vector2(
+                inputDirection.x * horizontalForceMultiplier,
+                verticalForceMultiplier
+            ) * maxJumpForce * powerRatio;
+
+            rb.linearVelocity = Vector2.zero;
+            rb.AddForce(jumpForce, ForceMode2D.Impulse);
+
+            isCharging = false;
+        }
+
+        //// ÁÂ¿ì ÀÌµ¿
+        //float moveInput = Input.GetAxisRaw("Horizontal");
+        //rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        //// Á¡ÇÁ
+        //if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+        //{
+        //    isJump = true;
+        //    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        //}
     }
 
     // ¶¥¿¡ ´ê¾Ò´ÂÁö È®ÀÎ
@@ -32,7 +70,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isJump = false;
+            isCharging = false;
         }
     }
 }
